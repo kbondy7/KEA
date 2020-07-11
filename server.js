4dfaad76a592f6056d34957539329f1428a87703
Kieran <k.bondman7@gmail.com>
2020-07-10 18:04:46 -0700

    adding the server and auth stuff

---
+++ server.js
@@ -0,0 +1,8 @@
+/* Load the HTTP library */
+var http = require("http");
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }).listen(8888);
